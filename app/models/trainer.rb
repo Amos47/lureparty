7f6468b21b4daedb28da50ba9eab2b07daab04e3
Amos47 <jordan.amos@shopify.com>
2016-07-17 23:51:32 -0400

    trainer show page update and presence validations

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,6 @@
 class Trainer < ApplicationRecord
   belongs_to :city, foreign_key: 'city_place_id', primary_key: 'place_id'
-  validates :name, uniqueness: true
-  validates :phone, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+  validates :phone, uniqueness: true, presence: true
+  validates :team, inclusion: { in: %w(Valor Mystic Instinct Harmony), message: "%{value} is not a valid team" }
 end
